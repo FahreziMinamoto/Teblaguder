fb11b2e985319481dc7175a98954d0ead618ee3f
FahreziMinamoto <rejikangroam@gmail.com>
2025-04-13 23:59:12 +0700

    Add files via upload

---
+++ game3.js
@@ -0,0 +1,28 @@
+function checkanswer(jawabanBenar) {
+    const inputUser = document.getElementById("answerinput").value.trim().toLowerCase();
+    const feedback = document.getElementById("feedback");
+  
+    if (inputUser === jawabanBenar.toLowerCase()) {
+      feedback.innerText = "Jawaban Benar!";
+      feedback.style.color = "green";
+      feedback.style.background = "white";
+      feedback.style.width = "300px";
+      feedback.style.maxWidth = "90%"
+      feedback.style.padding = "10px"
+      feedback.style.margin = "20px auto"
+  
+      // Redirect ke index5.html setelah 1 detik
+      setTimeout(() => {
+        window.location.href = "index5.html"
+      }, 1000);
+    } else {
+      feedback.innerText = "Jawaban Salah. Coba lagi!";
+      feedback.style.color = "red";
+      feedback.style.background = "white";
+      feedback.style.background = "white";
+      feedback.style.width = "300px";
+      feedback.style.maxWidth = "90%"
+      feedback.style.padding = "10px"
+      feedback.style.margin = "20px auto"
+    }
+  }
